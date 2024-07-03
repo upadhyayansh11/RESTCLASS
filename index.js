7869d261f2d2a0b711099c274dfcda9cc1e26835
Devansh Upadhyay <upadhyayansh11@gmail.com>
2024-07-03 13:51:22 +0530

    new files

--- index.js
+++ index.js
@@ -4,9 +4,10 @@ const app = express();
 const port = 8080;
  const path = require("path");
  const { v4:uuidv4 } = require("uuid");
+ const methodOverride = require("method-override");
 
  app.use(express.urlencoded({extended:true}));
-
+app.use(methodOverride("_method"));
 app.set("view engine","ejs");
 app.set("views",path.join(__dirname,"views"));
 
@@ -61,12 +62,18 @@ app.get("/posts/:id",(req,res)=>{
     let post = posts.find((p) => id === p.id);
     post.content = newContent ;
     console.log(post);
-    res.send("patch request working")
+    res.redirect("/posts");
  });
  app.get("/posts/:id/edit",(req,res)=>{
     let {id} = req.params;
     let post = posts.find((p) => id === p.id);
     res.render("edit.ejs",{ post });
+ })
+ app.delete("/posts/:id",(req,res)=>{
+    let {id} = req.params;
+     posts = posts.filter((p) => id !== p.id);
+
+    res.redirect("/posts");
  })
 
 
